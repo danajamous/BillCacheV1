182c247b1b79569a4c293ec5ae1c0b46eb0dd50a
Luke Young <luke.young@spatialfront.com>
2022-04-03 18:18:55 -0400

    Big fixes to the Total Amount to Pay function on the Saved Bills Page. Change trash icon to red color

--- src/app/saved-bills/saved-bills.component.ts
+++ src/app/saved-bills/saved-bills.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -10,65 +10,67 @@ import { Component, OnInit } from '@angular/core';
 
 export class SavedBillsComponent {
   amountTopayId: string = "";
-  amountTopayValue: string = "";
   checkBoxInputId: string = "";
-  totalAmountToPay:number=0;
+  totalAmountToPay: number = 0;
+  totalAmountToPayDisplay: string = "$0.00";
   data: Array<any>;
-  buttonDisabled:boolean=true;
-  numberOfCheckedBills:number=0;
+  buttonDisabled: boolean = true;
+  numberOfCheckedBills: number = 0;
   displayedColumns: string[] = ['id', 'billerName', 'serviceName', 'billingNumber', 'amountToPay', 'inquire'];
 
 
   
   constructor() {
-    this.data = [{ id: 1, billerName: 'T-Mobile', serviceName: "Prepaid", billingNumber: "202345555", amountToPay: 5.50 },
-    { id: 2, billerName: 'George Mason University ', serviceName: "Student Balance", billingNumber: "G-3788", amountToPay: 5000 },
-    { id: 3, billerName: 'Dominion Energy', serviceName: "Energy Bill", billingNumber: "7107 Alexandria", amountToPay: 220 },]
+    this.data = [
+      { id: 1, billerName: 'T-Mobile', serviceName: "Prepaid", billingNumber: "202345555", amountToPay: 5.50 },
+      { id: 2, billerName: 'George Mason University ', serviceName: "Student Balance", billingNumber: "G-3788", amountToPay: 5000 },
+      { id: 3, billerName: 'Dominion Energy', serviceName: "Energy Bill", billingNumber: "7107 Alexandria", amountToPay: 220 },
+    ]
+  }
+
+  formatDollarValue(n: number): string {
+    return `$${(Math.round(n * 100) / 100).toFixed(2)}`;
   }
 
 
-  deleteRow(id: any) {
+  deleteRow(element: any) {
     for (let i = 0; i < this.data.length; ++i) {
-      if (this.data[i].id === id) {
+      if (this.data[i].id === element.id) {
         this.data.splice(i, 1);
       }
     }
-    this.amountTopayId = "amountToPay" +id;
-    this.checkBoxInputId = "checkBox"+ id;
-    this.amountTopayValue=  (<HTMLInputElement>document.getElementById(this.amountTopayId)).value; 
+    this.checkBoxInputId = "checkBox" + element.id;
     if((<HTMLInputElement>document.getElementById(this.checkBoxInputId)).checked)
     {
-      this.totalAmountToPay -=  Number(this.amountTopayValue);
+      this.totalAmountToPay -= element.amountToPay;
       this.numberOfCheckedBills--;
-      if(this.numberOfCheckedBills ==0 )
-      this.buttonDisabled=true;
-   
+      if (this.numberOfCheckedBills == 0)
+        this.buttonDisabled = true;
     }
+    this.totalAmountToPayDisplay = this.formatDollarValue(this.totalAmountToPay);
   }
 
   inquire(element: any) {
     this.amountTopayId = "amountToPay" + element.id;
-    this.checkBoxInputId = "checkBox"+ element.id;
-    (<HTMLInputElement>document.getElementById(this.amountTopayId)).value = element.amountToPay;
+    this.checkBoxInputId = "checkBox" + element.id;
+    (<HTMLLabelElement>document.getElementById(this.amountTopayId)).textContent = this.formatDollarValue(element.amountToPay);
     (<HTMLInputElement>document.getElementById(this.checkBoxInputId)).removeAttribute('disabled');
   }
 
-  changeStatus(element:any,event: any){ 
-   this.checkBoxInputId = "checkBox"+element.id;
-   this.amountTopayId = "amountToPay" + element.id;
-   (<HTMLInputElement>document.getElementById(this.amountTopayId)).value = element.amountToPay;
-   if (event.target.checked){
-    this.totalAmountToPay+=element.amountToPay; 
-    this.numberOfCheckedBills++;
-    this.buttonDisabled=false;
+  toggleCheckbox(element: any, event: any) { 
+    if (event.target.checked) {
+     this.totalAmountToPay += element.amountToPay; 
+     this.numberOfCheckedBills++;
+     this.buttonDisabled = false;
+    }
+    else {
+     this.totalAmountToPay -= element.amountToPay;
+     this.numberOfCheckedBills--;
+     if (this.numberOfCheckedBills == 0)
+       this.buttonDisabled = true;
+    }
+    this.totalAmountToPayDisplay = this.formatDollarValue(this.totalAmountToPay);
    }
-   else{
-    this.totalAmountToPay-=element.amountToPay;
-    this.numberOfCheckedBills--;
-    if(this.numberOfCheckedBills ==0 )
-      this.buttonDisabled=true;
-   }
-  }
   
   payButtonClick(){
     if(confirm("redirect to payment method page ")) {
